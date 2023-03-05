31a728b8a82474e24ef212b5adadf52c7ab56e4a
Arpan Biswas <arpanbiswas97@gmail.com>
2023-03-06 03:01:55 +0530

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="speak",
-    version="0.1.0",
+    version="0.1.1",
     description="Speaks the text out loud using Google Text-to-Speech",
     author="Arpan Biswas",
     author_email="arpanbiswas97.com",
