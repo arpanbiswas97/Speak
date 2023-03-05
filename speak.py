d42bc68b19d205a3840844c012c42af64b986277
Arpan Biswas <arpanbiswas97@gmail.com>
2023-03-06 02:26:29 +0530

    Add speak.py

---
+++ speak.py
@@ -0,0 +1,42 @@
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+import argparse
+from io import BytesIO
+
+import gtts
+import pygame
+
+
+def speak(text: str):
+    """Speaks the text out loud using Google Text-to-Speech
+
+    Parameters
+    ----------
+    text : str
+        The text to speak
+
+    Returns
+    -------
+    None
+    """
+    tts = gtts.gTTS(text)
+    mp3_fp = BytesIO()
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(mp3_fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.wait(100)
+        pass
+
+
+parser = argparse.ArgumentParser("speak", description="Speaks the text out loud")
+parser.add_argument("text")
+args = parser.parse_args()
+
+text = args.text
+speak(text)
