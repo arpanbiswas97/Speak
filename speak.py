73d60b25583af16daa35be23a773cb33e59e3ab8
Arpan Biswas <arpanbiswas97@gmail.com>
2023-03-07 21:46:31 +0530

    Add option to choose language and variants

--- speak.py
+++ speak.py
@@ -8,24 +8,69 @@ from io import BytesIO
 import gtts
 import pygame
 
+LANGUAGE_MAPPINGS = {
+    "english": {
+        "local": ("en", "com"),
+        "au": ("en", "com.au"),
+        "uk": ("en", "co.uk"),
+        "us": ("en", "us"),
+        "ca": ("en", "ca"),
+        "in": ("en", "co.in"),
+        "ie": ("en", "ie"),
+        "za": ("en", "co.za"),
+    },
+    "french": {
+        "local": ("fr", "com"),
+        "ca": ("fr", "ca"),
+        "fr": ("fr", "fr"),
+    },
+    "mandarin": {
+        "cn": ("zh-CN", "com"),
+        "tw": ("zh-TW", "com"),
+    },
+    "portuguese": {
+        "local": ("pt", "com"),
+        "br": ("pt", "com.br"),
+        "pt": ("pt", "pt"),
+    },
+    "spanish": {
+        "local": ("es", "com"),
+        "es": ("es", "es"),
+        "mx": ("es", "com.mx"),
+        "us": ("es", "us"),
+    },
+}
 
-def speak(text: str):
-    """Speaks the text out loud using Google Text-to-Speech
+
+def speak(text: str, language: str = "english", variant: str = "local"):
+    """Speak the text out loud using Google Text-to-Speech.
 
     Parameters
     ----------
     text : str
-        The text to speak
-
-    Returns
-    -------
-    None
+        Text to speak.
+    language : str, optional
+        Language to use, by default "english"
+    variant : str, optional
+        Language variant to use, by default "local"
     """
-    tts = gtts.gTTS(text, lang="en", tld="us")
-    mp3_fp = BytesIO()
-    tts.write_to_fp(mp3_fp)
+
+    if language not in LANGUAGE_MAPPINGS:
+        raise ValueError(
+            f"Invalid language: {language}. Valid languages are: {list(LANGUAGE_MAPPINGS.keys())}"
+        )
+    if variant not in LANGUAGE_MAPPINGS[language]:
+        raise ValueError(
+            f"Invalid variant: {variant} for language {language}. Valid variants are: {list(LANGUAGE_MAPPINGS[language].keys())}"
+        )
+    lang, tld = LANGUAGE_MAPPINGS[language][variant]
+
+    tts = gtts.gTTS(text, lang=lang, tld=tld)
+    mp3_fp = BytesIO()  # In-memory file-like object
+    tts.write_to_fp(mp3_fp)  # Write the mp3 data to the file-like object
     mp3_fp.seek(0)
 
+    # Play the mp3 file
     pygame.mixer.init()
     pygame.mixer.music.load(mp3_fp)
     pygame.mixer.music.play()
@@ -34,14 +79,31 @@ def speak(text: str):
         pass
 
 
-def main():
-    parser = argparse.ArgumentParser("speak", description="Speaks the text out loud using Google Text-to-Speech")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "speak", description="Speaks the text out loud using Google Text-to-Speech"
+    )
     parser.add_argument("text")
+    parser.add_argument(
+        "-l",
+        "--language",
+        default="english",
+        help="Language to use (default: english)",
+        type=str,
+        choices=LANGUAGE_MAPPINGS.keys(),
+    )
+    parser.add_argument(
+        "-v",
+        "--variant",
+        default="local",
+        help="Language variant to use (default: local)",
+        type=str,
+        choices=set([k for d in LANGUAGE_MAPPINGS.values() for k in d.keys()]),
+    )
     args = parser.parse_args()
 
     text = args.text
-    speak(text)
+    language = args.language
+    variant = args.variant
 
-
-if __name__ == "__main__":
-    main()
+    speak(text, language, variant)
